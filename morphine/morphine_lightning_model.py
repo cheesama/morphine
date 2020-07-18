7b67d1abd2b19f7e4b1f4ae77567fe6144f1b3f1
Ubuntu <ltech@rasalt-dev.p1nvgwc5saqe3ass43yrm55tjc.syx.internal.cloudapp.net>
2020-07-18 15:55:56 +0000

    modify model arch for fixing loss value NaN2

--- morphine/morphine_lightning_model.py
+++ morphine/morphine_lightning_model.py
@@ -144,17 +144,16 @@ class MorphineClassifier(pl.LightningModule):
         tokens, intent_idx, entity_idx = batch
         intent_pred, entity_pred = self.forward(tokens)
 
-        if torch.isnan(intent_pred).sum().item() > 0:
-            print ('intent_pred error')
-            print (tokens)
-            print (intent_idx)
-            print (entity_idx)
-        if torch.isnan(entity_pred).sum().item() > 0:
-            print ('entity_pred error')
+        if torch.isnan(tokens).sum().item() > 0:
+            print ('tokens error')
         if torch.isnan(intent_idx).sum().item() > 0:
             print ('intent_idx error')
         if torch.isnan(entity_idx).sum().item() > 0:
             print ('entity_idx error')
+        if torch.isnan(intent_pred).sum().item() > 0:
+            print ('intent_pred error')
+        if torch.isnan(entity_pred).sum().item() > 0:
+            print ('entity_pred error')
 
         intent_acc = get_accuracy(intent_pred.argmax(1), intent_idx)[0]
         intent_f1 = f1_score(intent_pred.argmax(1), intent_idx)
