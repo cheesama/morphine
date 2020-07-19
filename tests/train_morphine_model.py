5a4fee5e1b7447d78c2efeadc12844e7c122b745
Ubuntu <ltech@rasalt-dev.p1nvgwc5saqe3ass43yrm55tjc.syx.internal.cloudapp.net>
2020-07-19 09:03:48 +0000

    modify training script

--- tests/train_morphine_model.py
+++ tests/train_morphine_model.py
@@ -5,8 +5,8 @@ import os, sys
 trainer.train(
     file_path="nlu.md",
     batch_size=256,
-    intent_optimizer_lr=5e-5,
-    entity_optimizer_lr=5e-5,
+    intent_optimizer_lr=1e-4,
+    entity_optimizer_lr=1e-4,
     gpu_num=0,
-    epochs=10
+    epochs=20
 )
